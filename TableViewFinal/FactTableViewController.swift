86af7f10a5b39a687cc444f3a120f556b89d2571
brianpoole <game.programmer@gmail.com>
2017-05-04 19:15:43 -0700

    solved

--- TableViewFinal/FactTableViewController.swift
+++ TableViewFinal/FactTableViewController.swift
@@ -9,4 +9,25 @@
 import UIKit
 
 class FactTableViewController: UITableViewController {
+    let factCellIdentifier: String = "FactCell"
+    var property: Planet!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = property.name
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return property.facts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: factCellIdentifier, for: indexPath) as! FactTableViewCell
+        cell.factTextView.text = property.facts[indexPath.row]
+        return cell
+    }
 }
--- TableViewFinal/PlanetTableViewController.swift
+++ TableViewFinal/PlanetTableViewController.swift
@@ -9,4 +9,57 @@
 import UIKit
 
 class PlanetTableViewController: UITableViewController {
+    let planetCellIdentifier: String = "PlanetCell"
+    var planets: [Planet] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        generatePlanetInfo()
+    }
+    
+    func generatePlanetInfo( ){
+        let earthFacts = [
+            "Earth is the only planet not named after a god.",
+            "Earth has a powerful magnetic field.",
+            "Earth was once believed to be the center of the universe."
+        ]
+        let earth = Planet(name: "Earth", numberOfMoons: 1, fullOrbit: 365.26, facts: earthFacts)
+        
+        let marsFacts = [
+            "Mars and Earth have approximately the same landmass.",
+            "Mars is home to the tallest mountain in the solar system.",
+            "Pieces of Mars have fallen to Earth."
+        ]
+        let mars = Planet(name: "Mars", numberOfMoons: 2, fullOrbit: 687.0, facts: marsFacts)
+        
+        planets = [earth, mars]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planets.count
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dest = segue.destination as? FactTableViewController, let indexPath = tableView.indexPathForSelectedRow{
+            dest.property = planets[indexPath.row]
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: planetCellIdentifier, for: indexPath) as! PlanetTableViewCell
+        cell.planetNameLabel.text = planets[indexPath.row].name
+        let moon_count = planets[indexPath.row].numberOfMoons
+        var moon_label = "Moons"
+        if moon_count == 1{
+            moon_label = "Moon"
+        }
+        cell.numberOfMoonsLabel.text = "\(moon_count) \(moon_label)"
+        cell.numberOfDaysFullOrbitLabel.text = "\(planets[indexPath.row].fullOrbit) days (full orbit)"
+        cell.numberOfFactsLabel.text = "\(planets[indexPath.row].facts.count) Facts"
+        return cell
+    }
 }
